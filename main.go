eb737562e547591273f9358a104bfe5832ac2823
Tung Dang <tungdq2@vng.com.vn>
2021-10-06 23:31:10 +0700

    recipe data model

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
-import "github.com/gin-gonic/gin"
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Recipe struct {
+	//swagger:ignore
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
 
 func main() {
     router := gin.Default()
